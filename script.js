bb2b15082efce65293d2a93c5ed621653f361c2e
kinchan222 <st4el158rm6@gmail.com>
2022-06-20 08:01:02 +0900

    全体のレスポンシブ開始

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-
-// $(function () {
-// 	$(window).scroll(function () {
-// 		const wHeight = $(window).height();
-// 		const scrollAmount = $(window).scrollTop();
-// 		$('.scrollanime').each(function () {
-// 			const targetPosition = $(this).offset().top;
-// 			if(scrollAmount > targetPosition - wHeight + 100) {
-// 				$(this).addClass("fadeInDown");
-// 			}
-// 		});
-// 	});
-// });
+ $(function () {
+ 	$(window).scroll(function () {
+ 		const wHeight = $(window).height();
+ 		const scrollAmount = $(window).scrollTop();
+ 		$('.scrollanime').each(function () {
+ 			const targetPosition = $(this).offset().top;
+ 			if(scrollAmount > targetPosition - wHeight + 100) {
+ 				$(this).addClass("fadeInDown");
+ 			}
+ 		});
+ 	});
+});
